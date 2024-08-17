b0f00bf02982909c1ab50e069fbbb8644e93d0ed
BuildTools <unconfigured@null.spigotmc.org>
2024-08-18 04:21:41 +0800

    沒啥好說的

---
+++ src/main/java/com/github/nalamodikk/datagen/ModBlockStateProvider.java
@@ -0,0 +1,34 @@
+package com.github.nalamodikk.datagen;
+
+import com.github.nalamodikk.magical_industry;
+import com.github.nalamodikk.block.ModBlocks;
+import net.minecraft.data.PackOutput;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.client.model.generators.BlockStateProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModBlockStateProvider extends BlockStateProvider {
+    public ModBlockStateProvider(PackOutput output, ExistingFileHelper exFileHelper) {
+        super(output, magical_industry.MOD_ID, exFileHelper);
+    }
+
+    @Override
+    protected void registerStatesAndModels() {
+        blockWithItem(ModBlocks.MANA_BLOCK);
+
+
+        /*
+        blockWithItem(ModBlocks.SAPPHIRE_ORE);
+        blockWithItem(ModBlocks.DEEPSLATE_SAPPHIRE_ORE);
+        blockWithItem(ModBlocks.END_STONE_SAPPHIRE_ORE);
+        blockWithItem(ModBlocks.NETHER_SAPPHIRE_ORE);
+
+         */
+
+    }
+
+    private void blockWithItem(RegistryObject<Block> blockRegistryObject) {
+        simpleBlockWithItem(blockRegistryObject.get(), cubeAll(blockRegistryObject.get()));
+    }
+}
