c79f18c1cc7d5402bc65973c61602d469d815714
BuildTools <unconfigured@null.spigotmc.org>
2024-08-18 23:42:56 +0800

    沒啥好說的

--- src/main/java/com/github/nalamodikk/item/ModCreativeModTabs.java
+++ src/main/java/com/github/nalamodikk/item/ModCreativeModTabs.java
@@ -14,18 +14,28 @@ public class ModCreativeModTabs {
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
             DeferredRegister.create(Registries.CREATIVE_MODE_TAB , MagicalIndustryMod.MOD_ID);
 
-public static final RegistryObject<CreativeModeTab> MAGICAL_INDUSTRY_TAB = CREATIVE_MODE_TABS.register("magical_industry_tab",
+    public static final RegistryObject<CreativeModeTab> MAGICAL_INDUSTRY_ITEMS_TAB = CREATIVE_MODE_TABS.register("magical_industry_items_tab",
         () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.MANA_DUST.get()))
-                .title(Component.translatable("creativetab.magical_industry_tab"))
+                .title(Component.translatable("creativetab.magical_industry_items"))
                 .displayItems((pParameters, pOutput) -> {
                     pOutput.accept(ModItems.MANA_DUST.get());
                     pOutput.accept(ModItems.MANA_INGOT.get());
 
 
-                    pOutput.accept(ModBlocks.MANA_BLOCK.get());
-
                 })
                 .build());
+
+
+
+    public static final RegistryObject<CreativeModeTab> MAGICAL_INDUSTRY_BLOCKS_TAB = CREATIVE_MODE_TABS.register("magical_industry_blocks_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModBlocks.MANA_BLOCK.get()))
+                    .title(Component.translatable("creativetab.magical_industry_blocks"))
+                    .displayItems((pParameters, pOutput) -> {
+
+                        pOutput.accept(ModBlocks.MANA_BLOCK.get());
+
+                    })
+                    .build());
 
     public static void register(IEventBus eventBus) {
         CREATIVE_MODE_TABS.register(eventBus);
