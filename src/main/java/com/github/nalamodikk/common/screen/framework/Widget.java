b32a01b988296300bfdd62ad3a09bcc3c65db78a
BuildTools <unconfigured@null.spigotmc.org>
2025-06-04 22:27:06 +0800

    1.21.1更新

---
+++ src/main/java/com/github/nalamodikk/common/screen/framework/Widget.java
@@ -0,0 +1,9 @@
+package com.github.nalamodikk.common.screen.framework;
+
+import net.minecraft.client.gui.GuiGraphics;
+
+public interface Widget {
+    void render(GuiGraphics graphics, int mouseX, int mouseY);
+    boolean mouseClicked(int mouseX, int mouseY, int button);
+    void mouseReleased(int mouseX, int mouseY, int button);
+}
