d9ef2b58079291c6e200143c2e6d16d0d749c32e
BuildTools <unconfigured@null.spigotmc.org>
2025-06-04 17:49:50 +0800

    已移除 PlatformClientHelper 架構，因目前僅支援 NeoForge

--- src/main/java/com/github/nalamodikk/client/platform/PlatformClientHelper.java
+++
@@ -1,8 +0,0 @@
-package com.github.nalamodikk.client.platform;
-
-
-import com.github.nalamodikk.common.network.packet.server.manatool.ManaUpdatePacket;
-
-public interface PlatformClientHelper {
-        void handleManaPacket(ManaUpdatePacket packet);
-}
