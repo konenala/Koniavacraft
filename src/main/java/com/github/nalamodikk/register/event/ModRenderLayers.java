e0dd8867ba21f3f1216f3fe7fe67980eeeae03d1
BuildTools <unconfigured@null.spigotmc.org>
2025-06-06 04:30:58 +0800

    1.21.1緊急修復面朝向工具踢出玩家bug

--- src/main/java/com/github/nalamodikk/register/event/ModRenderLayers.java
+++ src/main/java/com/github/nalamodikk/register/event/ModRenderLayers.java
@@ -3,10 +3,16 @@ package com.github.nalamodikk.register.event;
 import com.github.nalamodikk.MagicalIndustryMod;
 import com.github.nalamodikk.common.block.mana_generator.geckolib.ManaGeneratorRenderer;
 import com.github.nalamodikk.register.ModBlockEntities;
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.MultiBufferSource;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.client.event.ClientTickEvent;
 import net.neoforged.neoforge.client.event.EntityRenderersEvent;
+import net.neoforged.neoforge.client.event.RenderLevelStageEvent;
+
 @EventBusSubscriber(modid = MagicalIndustryMod.MOD_ID, value = Dist.CLIENT, bus = EventBusSubscriber.Bus.MOD)
 
 public class ModRenderLayers {
@@ -15,7 +21,5 @@ public class ModRenderLayers {
     public static void registerRenderers(final EntityRenderersEvent.RegisterRenderers event) {
         event.registerBlockEntityRenderer(ModBlockEntities.MANA_GENERATOR_BE.get(), ManaGeneratorRenderer::new);
     }
-
-
 
 }
